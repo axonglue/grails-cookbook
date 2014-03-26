efcc2a969c0a693397920fc922975de58b382716
Robert Sternberg <robert.sternberg@cloudreach.co.uk>
2014-03-26 10:31:29 +0000

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Robert Sternberg"
+maintainer_email "axonglue@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures grails"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends "java"
+depends "ark"
